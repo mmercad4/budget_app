33ab74585465ee4b7d7ad8f7f94ed8d7310444e7
Marco Mercado <marco@stonepawtraining.com>
2023-09-28 14:49:15 -0400

    Added expenses array and updated ExpenseItem component to receive props

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,8 +1,14 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <>
-      <h2>Expense item!</h2>
-    </>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
   );
 }
 
